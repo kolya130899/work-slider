1cde2d5ececa965d3402d4b0973c7e09bb7cb5c8
kolya130899 <kolyawote75@gmail.com>
2018-11-21 19:14:14 +0200

    v.1.0

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*$('.slider').slick({
+	dots: false,
+	infinite: true,
+	slide: "div",
+	prevArrow: ".prev",
+	nextArrow: ".next"
+});*/
+$('.slider').slick({
+	dots: false,
+	infinite: true,
+	slide: "div",
+	prevArrow: ".prev",
+	nextArrow: ".next",
+	centerMode: true,
+	centerPadding: '50px',
+	slidesToShow: 3,
+	slidesToScroll: 1
+  /*dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slide: "div",
+  prevArrow: ".prev",
+  nextArrow: ".next"*/
+});
